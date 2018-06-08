474b11128ab2742a2ba0621e54452720662ca403
LazyDie <1114170383@qq.com>
2018-06-08 13:59:31 +0800

    Redis设置数据有效期

--- demo1/src/main/java/demo/redisTest/LinkedRedis.java
+++ demo1/src/main/java/demo/redisTest/LinkedRedis.java
@@ -6,8 +6,14 @@ import redis.clients.jedis.Jedis;
  * Created by TIAN on 2018/5/30.
  */
 public class LinkedRedis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Jedis jedis = new Jedis("192.168.198.141",6379);
-        System.out.println(jedis.get("hello1"));
+        jedis .setex("tian",10, "风");
+        System.out.println(jedis.get("tian"));
+        Thread.sleep(5000);
+        System.out.println("5秒后："+jedis.get("tian"));
+        Thread.sleep(5000);
+        System.out.println("10秒后："+jedis.get("tian"));
+        //System.out.println(jedis.get("hello1"));
     }
 }
