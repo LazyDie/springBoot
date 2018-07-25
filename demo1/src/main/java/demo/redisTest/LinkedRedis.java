61264f220aedf7b698240976ce59bad82e62cefc
LazyDie <1114170383@qq.com>
2018-07-25 16:16:13 +0800

    11

--- demo1/src/main/java/demo/redisTest/LinkedRedis.java
+++ demo1/src/main/java/demo/redisTest/LinkedRedis.java
@@ -9,12 +9,15 @@ import redis.clients.jedis.Jedis;
 public class LinkedRedis {
     public static void main(String[] args) throws InterruptedException {
         Jedis jedis = new Jedis("192.168.198.141",6379);
-        jedis .setex("tian",10, "风");
+        jedis.set("tian","IDWORKER.GLOBAL_INCR_ID_WORKER");
+        jedis.incr("IDWORKER.GLOBAL_INCR_ID_WORKER");
+        System.out.println(jedis.get("IDWORKER.GLOBAL_INCR_ID_WORKER"));
+       /* jedis .setex("tian",10, "风");
         System.out.println(jedis.get("tian"));
         Thread.sleep(5000);
         System.out.println("5秒后："+jedis.get("tian"));
         Thread.sleep(5000);
         System.out.println("10秒后："+jedis.get("tian"));
-        //System.out.println(jedis.get("hello1"));
+        //System.out.println(jedis.get("hello1"));*/
     }
 }
